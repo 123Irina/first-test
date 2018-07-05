69e56532fab05bb356ca58b387ac827aa9d3f50a
Ira <irina.kalmykova83@gmail.com>
2018-07-05 22:17:10 +0300

    first_js

---
+++ lesson_1/js.js
@@ -0,0 +1,12 @@
+alert('Я люблю БОРЩ!')
+var admin,
+    name;
+name = "Василий";
+admin = name;
+alert(admin);
+var planet;
+planet = 'Земля';
+var guest = 'Петя';
+var userName = prompt('Как Вас зовут?');
+alert('Hello! ' + userName);
+
