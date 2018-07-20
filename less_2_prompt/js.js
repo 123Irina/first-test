b33be5687f0a68a155b554836edd53a8e142326a
Ira <irina.kalmykova83@gmail.com>
2018-07-20 18:23:23 +0300

    lesson2

---
+++ less_2_prompt/js.js
@@ -0,0 +1,18 @@
+"use strict";
+var userName = prompt('Кто пришел?');
+
+if (userName == 'Админ') {
+    var password = prompt('Пароль?');
+    if (password == 'Черный Властелин') {
+        alert('Добро пожаловать!')
+    }
+    else if (password == null) {
+        alert( 'Вход отменен' )
+    }
+    else alert( "Пароль неверен" )
+}
+else if (userName == null) {
+    alert('Вход отменен');
+}
+    else alert('Я Вас не знаю');
+
