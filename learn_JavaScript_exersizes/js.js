94c79a4391dcba9f1834507b4e8b3c3d5648f633
Ira <irina.kalmykova83@gmail.com>
2018-07-29 15:06:02 +0300

    simple

--- learn_JavaScript_exersizes/js.js
+++ learn_JavaScript_exersizes/js.js
@@ -1,1 +1,25 @@
-alert('Я – JavaScript!');
+'use strict'
+
+var str = prompt( 'Write text' );
+str = str.toLowerCase();
+var viagra = 'viagra',
+    xxx = 'xxx';
+function checkSpam(str) {
+    if (~str.indexOf("viagra")) {
+        alert( 'SPAM' );
+    }
+    if (~str.indexOf("xxx")) {
+        alert( 'SPAM' );
+    }
+}
+checkSpam(str);
+//
+// function checkSpam(str) {
+//     var lowerStr = str.toLowerCase();
+//
+//     return !!(~lowerStr.indexOf('viagra') || ~lowerStr.indexOf('xxx'));
+// }
+//
+// alert( checkSpam('buy ViAgRA now') );
+// alert( checkSpam('free xxxxx') );
+// alert( checkSpam("innocent rabbit") );
