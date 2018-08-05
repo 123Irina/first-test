62be639fde9f0bdc8aced8d6276d9986af395e98
Ira <irina.kalmykova83@gmail.com>
2018-08-05 23:13:32 +0300

    tabs

---
+++ Tabs/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+    $('button').click(function () {
+        $('.hidden').removeClass("hidden");
+    });
+       //переключение Табов
+
+    $('nav .tabs').click(function () {
+        if (!$(this).hasClass('is-checked')) {
+            $('.tabs.is-checked').removeClass('is-checked');
+            $(this).addClass('is-checked');
+        }
+    });
+
+//filter with isotope
+
+    var $isotopeGrid = $('.grid');
+
+    $isotopeGrid.isotope({
+        itemSelector: '.element-item',
+        percentPosition: true
+    });
+    $('.filters button').click(function () {
+        var filterValue = $(this).data('filter');
+        console.log(filterValue);
+        $isotopeGrid.isotope({
+            filter: filterValue
+        });
+    });
+
+});
